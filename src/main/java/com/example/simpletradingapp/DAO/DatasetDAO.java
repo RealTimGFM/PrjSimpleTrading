6877530781df398a9d6a1943bd788d8fa20972fd
ALPHVMANE <alphxrizen@gmail.com>
2025-04-11 14:08:25 -0400

    DatasetDAO
    Configuration of the DatasetDAO implementation

---
+++ src/main/java/com/example/simpletradingapp/DAO/DatasetDAO.java
@@ -0,0 +1,26 @@
+package com.example.simpletradingapp.DAO;
+import com.example.simpletradingapp.model.StockDataset;
+import java.util.*;
+
+public interface DatasetDAO {
+    /**
+     * Find company information
+     * @param symbol in the stock code
+     * @return the stockdataset or null if not found
+     */
+    List<StockDataset> findBySymbol(String symbol);
+
+    /**
+     * Find closed stock by date
+     * @param date in the stock code
+     * @return the close stock (StockDataset) or null if not found
+     */
+    List<StockDataset> findCloseByDate(Date date);
+    /**
+     * METHOD OVERLOAD
+     * Find closed stock by date and by symbol
+     * @param date in the stock code
+     * @return the close stock (StockDataset) or null if not found
+     */
+    StockDataset findCloseByDate(Date date, String symbol);
+}
